370c6f58e82cbac7c3a8431b34ba8eadc199c1fa
Paweł Ryba <pawel.ryba@ceneo.pl>
2016-05-15 18:24:24 +0200

    Get file list in app root folder from GDrive

--- DriveCrypt/Form1.cs
+++ DriveCrypt/Form1.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Windows.Forms;
 using Google.Apis.Auth.OAuth2;
@@ -14,57 +14,82 @@ namespace DriveCrypt
 {
     public partial class Form1 : Form
     {
+        public const string FolderName = "DriveCrypt";
+
         private readonly string[] _accessScopes = {DriveService.Scope.Drive};
         private UserCredential _credential;
+
+        private string _folderId;
+        private IEnumerable<Google.Apis.Drive.v3.Data.File> _files; 
 
         public Form1()
         {
             InitializeComponent();
+
+            Authorize();
+            MaintainMainFolder();
+            GetFiles();
         }
 
-        private async void LogIn_Click(object sender, EventArgs e)
+        private void Authorize()
         {
             using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
             {
                 var credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
                 credPath = Path.Combine(credPath, ".credentials/drive-crypt-auth.json");
 
-                _credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                _credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.Load(stream).Secrets,
                     _accessScopes,
                     "user",
                     CancellationToken.None,
-                    new FileDataStore(credPath, true));
+                    new FileDataStore(credPath, true)).Result;
             }
-
-            MaintainMainFolder();
         }
 
-        private async void MaintainMainFolder()
+        private void MaintainMainFolder()
         {
-            const string folderName = "DriveCrypt";
-
             var service = new DriveService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = _credential,
                 ApplicationName = "DriveCrypt",
             });
 
-            var fileList = await service.Files.List().ExecuteAsync();
+            var fileList = service.Files.List().Execute();
 
-            if(fileList.Files.All(x => x.Name != folderName))
+            if (fileList.Files.All(x => x.Name != FolderName))
             {
                 var fileMetadata = new Google.Apis.Drive.v3.Data.File
                 {
-                    Name = folderName,
+                    Name = FolderName,
                     MimeType = "application/vnd.google-apps.folder"
                 };
 
                 var request = service.Files.Create(fileMetadata);
                 request.Fields = "id";
 
-                await request.ExecuteAsync();
+                _folderId = request.Execute().Id;
             }
+            else
+            {
+                _folderId = fileList.Files.First(x => x.Name == FolderName).Id;
+            }
+        }
+
+        private async void GetFiles()
+        {
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = _credential,
+                ApplicationName = "DriveCrypt",
+            });
+
+            var request = service.Files.List();
+            request.Q = $"'{_folderId}' in parents";
+
+            var response = await request.ExecuteAsync();
+
+            _files = response.Files;
         }
     }
 }
